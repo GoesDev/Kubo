df070d7fa6bc36486b545fbeea143ac8dcc74168
Júlio Gois <129470014+GoesDev@users.noreply.github.com>
2025-09-19 15:09:32 -0300

    setup inicial

---
+++ kubo_app/main..py
@@ -0,0 +1,9 @@
+cond = True
+
+while cond:
+
+    print('Bem vindo ao Kubo!')
+    print('[1] Abrir Conta - [2] Visutalizar Contas - [3] Deletar Conta')
+    var = input(': ')
+    if var == '1':
+        cond = False
