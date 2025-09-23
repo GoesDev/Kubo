ae1ae90c96e8dc55a7fe476df81e7c27d54160db
Maria Eduarda Gomes <marig.gomes168@gmail.com>
2025-09-22 22:54:08 -0300

    feat: adicionando flask e rotas para acessar o servidor

---
+++ kubo_app/main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/formulario.html')
+def formulario():
+    return render_template('formulario.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
