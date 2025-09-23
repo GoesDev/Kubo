6c5fc6e7e4a1dac2abc01a3151923be3ef8b1277
Júlio Gois <129470014+GoesDev@users.noreply.github.com>
2025-09-22 23:07:27 -0300

    feat: adicionado funcoes de validacao senha e cripgrafia

--- kubo_app/core/auth_manager.py
+++ kubo_app/core/auth_manager.py
@@ -1,4 +1,5 @@
 import re
+import bcrypt
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
 
@@ -6,7 +7,7 @@ PASSWORD_REGEX = re.compile(
     r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
     )
 
-def check_email(email: str) -> bool:
+def validate_email(email: str) -> bool:
     """
     Verifica se a string de email fornecida corresponde ao padrão Regex.
     """
@@ -16,7 +17,7 @@ def check_email(email: str) -> bool:
     # Versão concisa: usa bool() para garantir True/False
     return bool(EMAIL_REGEX.fullmatch(email))
 
-def check_password(password: str) -> bool:
+def validate_password(password: str) -> bool:
     """
     Verifica se a password atende aos requisitos de segurança.
 
@@ -30,3 +31,39 @@ def check_password(password: str) -> bool:
         return False
 
     return bool(PASSWORD_REGEX.fullmatch(password))
+
+def hash_password(password: str) -> bytes:
+    """
+    Criptografa a senha usando bcrypt.
+    
+    O bcrypt gera um 'salt' automaticamente e o inclui no hash final.
+    O 'salt' é uma string aleatória que torna o hash de senhas iguais diferente,
+    protegendo contra ataques de tabela arco-íris.
+    
+    Args:
+        senha (str): A senha em texto puro.
+    
+    Returns:
+        bytes: A senha criptografada (hash).
+    """
+
+    password_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password_bytes, salt)
+
+    return hashed_password
+
+def check_password(password_to_check: str, hashed_password: bytes) -> bool:
+    """
+    Verifica se a senha digitada corresponde ao hash salvo.
+    
+    Args:
+        password_to_check (str): A senha em texto puro digitada pelo usuário.
+        hashed_password (bytes): O hash da senha salvo no banco de dados.
+        
+    Returns:
+        bool: True se as senhas coincidirem, False caso contrário.
+    """
+
+    password_bytes_to_check = password_to_check.encode('utf-8')
+    return bcrypt.checkpw(password_bytes_to_check, hashed_password)
--- kubo_app/fake_main.py
+++ kubo_app/fake_main.py
@@ -1,8 +1,9 @@
-# from core.auth_manager import validar_email
-from core.constants import DB_FILE, SQL_INSERT_INTO_ACCOUNTS, SQL_DELETE_ACCOUNTS_BY_ID, SQL_SELECT_ALL_ACCOUNTS
-from database.db_manager import connect_db, delete_by_id, select_all, create_table, insert_into
+from core.auth_manager import check_email, check_password
+# from core.constants import DB_FILE, SQL_INSERT_INTO_ACCOUNTS, SQL_DELETE_ACCOUNTS_BY_ID, SQL_SELECT_ALL_ACCOUNTS
+# from database.db_manager import connect_db, delete_by_id, select_all, create_table, insert_into
 
-conexao = connect_db(DB_FILE)
+
+# conexao = connect_db(DB_FILE)
 
 # delete_by_id(conexao, SQL_DELETE_ACCOUNTS_BY_ID, '1')
 
@@ -14,9 +15,11 @@ conexao = connect_db(DB_FILE)
 # create_table(conexao, SQL_CREATE_ACCOUNTS_TABLE)
 
 
-all_accounts = select_all(conexao, SQL_SELECT_ALL_ACCOUNTS)
+# all_accounts = select_all(conexao, SQL_SELECT_ALL_ACCOUNTS)
 
-for account in all_accounts:
-    print(account)
+# for account in all_accounts:
+#     print(account)
 
-# print(f"teste@email.com: {validar_email('teste@email.com')}")
+# print(f"teste@email.com: {check_email('teste@email.com')}")
+# print(f"Senha@123: {check_password('Senha@123')}")
+# print(f"senhafraca: {check_password('senhafF1@raca')}")
