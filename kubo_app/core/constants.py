252e0e565883a92801176d21d85f814429957798
Júlio Gois <129470014+GoesDev@users.noreply.github.com>
2025-09-22 22:35:28 -0300

    feat/refact: adicionando tabela accounts ao banco e realizando alguns testes com as funcoes

--- kubo_app/core/constants.py
+++ kubo_app/core/constants.py
@@ -5,11 +5,26 @@ DB_FILE = 'kubo_app_database.db'
 LIMITE_ALERTA_ORCAMENTO = 0.80  # 80% do orçamento consumido dispara um alerta
 MOEDA_PADRAO = 'BRL'            # Código da moeda padrão (Real Brasileiro)
 
+#------------------------------------------------------------------------
+
 SQL_CREATE_USERS_TABLE = """CREATE TABLE IF NOT EXISTS users (
 id INTEGER PRIMARY KEY, name TEXT NOT NULL, email_login TEXT NOT NULL, password TEXT NOT NULL)"""
 
 SQL_INSERT_INTO_USERS = """INSERT INTO users (name, email_login, password) VALUES (?, ?, ?)"""
 
-SQL_SELECT_ALL_USERS = """SELECT id, name, email_login, password FROM users"""
+SQL_SELECT_ALL_USERS = """SELECT name, email_login, password FROM users"""
 
 SQL_DELETE_USERS_BY_ID = """DELETE FROM users WHERE id = ?"""
+
+#------------------------------------------------------------------------
+
+SQL_CREATE_ACCOUNTS_TABLE = """CREATE TABLE IF NOT EXISTS accounts (
+id INTEGER PRIMARY KEY, name TEXT NOT NULL, type TEXT NOT NULL, current_balance REAL NOT NULL,
+color TEXT NOT NULL)"""
+
+SQL_INSERT_INTO_ACCOUNTS = """INSERT INTO accounts (name, type, current_balance, 
+color) VALUES (?, ?, ?, ?)"""
+
+SQL_SELECT_ALL_ACCOUNTS = """SELECT name, type, current_balance, color FROM accounts"""
+
+SQL_DELETE_ACCOUNTS_BY_ID = """DELETE FROM accounts WHERE id = ?"""
--- kubo_app/fake_main.py
+++ kubo_app/fake_main.py
@@ -1,22 +1,22 @@
 # from core.auth_manager import validar_email
-from core.constants import DB_FILE, SQL_DELETE_USERS_BY_ID, SQL_SELECT_ALL_USERS
-from database.db_manager import connect_db, delete_by_id, select_all
+from core.constants import DB_FILE, SQL_INSERT_INTO_ACCOUNTS, SQL_DELETE_ACCOUNTS_BY_ID, SQL_SELECT_ALL_ACCOUNTS
+from database.db_manager import connect_db, delete_by_id, select_all, create_table, insert_into
 
 conexao = connect_db(DB_FILE)
 
-# delete_by_id(conexao, SQL_DELETE_USERS_BY_ID, '3')
+# delete_by_id(conexao, SQL_DELETE_ACCOUNTS_BY_ID, '1')
 
-# user = ('Kassandra', 'rata@kassandra.com', '777')
+# account = ('Banco pra Frentex', 'Poupanca', '200', 'Verde')
 
-# insert_into(conexao, SQL_INSERT_INTO_USERS, user)
-
-
-# create_table(conexao, SQL_CREATE_USERS_TABLE)
+# insert_into(conexao, SQL_INSERT_INTO_ACCOUNTS, account)
 
 
-# all_users = select_all(conexao, SQL_SELECT_ALL_USERS)
+# create_table(conexao, SQL_CREATE_ACCOUNTS_TABLE)
 
-# for user in all_users:
-#     print(user)
+
+all_accounts = select_all(conexao, SQL_SELECT_ALL_ACCOUNTS)
+
+for account in all_accounts:
+    print(account)
 
 # print(f"teste@email.com: {validar_email('teste@email.com')}")
