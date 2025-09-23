fd9366f947d79ea211450e1e6c8fa594b68960c0
Júlio Gois <129470014+GoesDev@users.noreply.github.com>
2025-09-22 22:45:37 -0300

    feat/refact: adicionado validacao de senha conforme regra de negócio

--- kubo_app/core/auth_manager.py
+++ kubo_app/core/auth_manager.py
@@ -2,7 +2,11 @@ import re
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
 
-def validar_email(email: str) -> bool:
+PASSWORD_REGEX = re.compile(
+    r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
+    )
+
+def check_email(email: str) -> bool:
     """
     Verifica se a string de email fornecida corresponde ao padrão Regex.
     """
@@ -11,3 +15,18 @@ def validar_email(email: str) -> bool:
     
     # Versão concisa: usa bool() para garantir True/False
     return bool(EMAIL_REGEX.fullmatch(email))
+
+def check_password(password: str) -> bool:
+    """
+    Verifica se a password atende aos requisitos de segurança.
+
+    Args:
+        password (str): A password a ser validada.
+
+    Returns:
+        bool: True se a password for válida, False caso contrário.
+    """
+    if not password or len(password) < 8:
+        return False
+
+    return bool(PASSWORD_REGEX.fullmatch(password))
