9e2878efbfb3fb8e798de0016a154a0333998f49
Júlio Gois <129470014+GoesDev@users.noreply.github.com>
2025-09-23 22:20:28 -0300

    temp: subindo arquivo temporário de testes

--- kubo_app/fake_main.py
+++ kubo_app/fake_main.py
@@ -1,15 +1,24 @@
-from core.auth_manager import check_email, check_password
-# from core.constants import DB_FILE, SQL_INSERT_INTO_ACCOUNTS, SQL_DELETE_ACCOUNTS_BY_ID, SQL_SELECT_ALL_ACCOUNTS
-# from database.db_manager import connect_db, delete_by_id, select_all, create_table, insert_into
+from core.auth_manager import validade_new_user, validate_login_email
 
+resultado = validade_new_user('Usuário', 'emaul@email.com.br.net', 'P@ssw0rd')
+print(resultado)
+
+result = validate_login_email('email@email.com.br', 'P@ssw0rd')
+
+print(result)
+
+
+# for i in range(11):
+#     delete_by_id(connect_db(DB_FILE), SQL_DELETE_USERS_BY_ID, str(i))
+
+# vamo = validade_new_user('Duarda', 'email@daedra.net', 'Torr3@$s')
+# print(vamo)
+
+# print(validate_login_email('email@email.com', ''))
 
 # conexao = connect_db(DB_FILE)
 
 # delete_by_id(conexao, SQL_DELETE_ACCOUNTS_BY_ID, '1')
-
-# account = ('Banco pra Frentex', 'Poupanca', '200', 'Verde')
-
-# insert_into(conexao, SQL_INSERT_INTO_ACCOUNTS, account)
 
 
 # create_table(conexao, SQL_CREATE_ACCOUNTS_TABLE)
@@ -23,3 +32,20 @@ from core.auth_manager import check_email, check_password
 # print(f"teste@email.com: {check_email('teste@email.com')}")
 # print(f"Senha@123: {check_password('Senha@123')}")
 # print(f"senhafraca: {check_password('senhafF1@raca')}")
+
+ # 1. Hashing
+# original_password = "GOES@8833a!"
+# hashed_password = hash_password(original_password)
+
+# print(f"Senha original: {original_password}")
+# print(f"Senha hash (salva no DB): {hashed_password}")
+
+# # 2. Verificação (simulando um login)
+# print("\nSimulando login...")
+# correct_password = "GOES@8833a!"
+# incorrect_password = "SenhaIncorreta"
+
+# # Deve ser True
+# print(f"Verificando senha correta: {check_password(correct_password, hashed_password)}")
+# # Deve ser False
+# print(f"Verificando senha incorreta: {check_password(incorrect_password, hashed_password)}")
